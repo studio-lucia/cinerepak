f955d36249ce3f7ea609ccf5ec7969195bbfea45
Misty De Meo <mistydemeo@gmail.com>
2017-08-04 21:41:04 -0700

    Finish FILMHeader parser

--- src/lib.rs
+++ src/lib.rs
@@ -12,17 +12,26 @@ fn uint16_from_bytes(bytes : [u8; 2]) -> u16 {
 struct FILMHeader {
     // Always 'FILM'
     signature: String,
-    length: u32,
+    length: usize,
     version: String,
+    unknown: Vec<u8>,
     fdsc: FDSC,
     stab: STAB,
 }
 
-// impl FILMHeader {
-//     fn parse() -> FILMHeader {
-
-//     }
-// }
+impl FILMHeader {
+    fn parse(data : &[u8]) -> FILMHeader {
+        let length = uint32_from_bytes([data[4], data[5], data[6], data[7]]) as usize;
+        return FILMHeader {
+            signature: String::from_utf8(data[0..3].to_vec()).unwrap(),
+            length: length,
+            version: String::from_utf8(data[8..11].to_vec()).unwrap(),
+            unknown: data[12..15].to_vec(),
+            fdsc: FDSC::parse(&data[16..47]),
+            stab: STAB::parse(&data[48..length]),
+        }
+    }
+}
 
 struct FDSC {
     // Always 'FDSC'
